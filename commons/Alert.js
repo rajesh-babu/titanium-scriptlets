9b5384d3ebd8eefe2dad3a3bb0597cd68365a620
Rajesh Babu <trcrajesh_be@yahoo.com>
2014-11-02 14:32:25 -0500

    Create Alert.js

---
+++ commons/Alert.js
@@ -0,0 +1,5 @@
+var a = Titanium.UI.createAlertDialog({
+				title : 'Oops!',
+				message : "You need to add your Facebook account in your iOS settings before you can start sharing"
+			});
+			a.show();
